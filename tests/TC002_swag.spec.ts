1b6dc283d59ce57ea3ad9a6b84ecae5ce3115666
prabhakar721 <prabhas9102@gmail.com>
2026-09-18 21:43:59 +0530

    Thirdpush

--- tests/TC002_swag.spec.ts
+++ tests/TC002_swag.spec.ts
@@ -4,7 +4,6 @@ test('test', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
   await expect(page.locator('[data-test="username"]')).toBeVisible();
-  await page.locator('[data-test="username"]').fill('standard_user');
   await page.locator('[data-test="password"]').click();
   await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
